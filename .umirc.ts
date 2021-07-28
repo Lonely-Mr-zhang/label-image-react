4051874f902ab26881ff31bc37cfcbc0ae379234
张聪龙 <zhangconglong@ingbaobei.com>
2021-07-28 12:01:16 +0800

    mock数据

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  layout: {},
   nodeModulesTransform: {
     type: 'none',
   },
+  extraBabelPresets: [
+    [
+      '@babel/preset-typescript',
+      {
+        allowDeclareFields: true,
+      },
+    ],
+  ],
   routes: [
     { path: '/', component: '@/pages/index' },
+    { path: '/DoLabel', component: '@/pages/LabelImage/DoLabel/index' },
+    {
+      path: '/LabelTaskManage',
+      component: '@/pages/LabelImage/LabelTaskManage/index',
+    },
   ],
   fastRefresh: {},
 });
