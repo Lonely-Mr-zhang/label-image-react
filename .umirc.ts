7bdd8cb72348ba2de5e5dadc29ef86cd84c55896
张聪龙 <zhangconglong@ingbaobei.com>
2021-09-13 18:36:52 +0800

    update

--- .umirc.ts
+++ .umirc.ts
@@ -14,11 +14,46 @@ export default defineConfig({
     ],
   ],
   routes: [
-    { path: '/', component: '@/pages/index' },
-    { path: '/DoLabel', component: '@/pages/LabelImage/DoLabel/index' },
+    // { path: '/', component: '@/pages/index' },
+    // { path: '/DoLabel', component: '@/pages/LabelImage/DoLabel/index' },
+    // {
+    //   path: '/LabelTaskManage',
+    //   component: '@/pages/LabelImage/LabelTaskManage/index',
+    // },
+    // {
+    //   path: '/label_image',
+    //   name: '标注系统',
+    //   icon: 'TagsOutlined',
+    //   routes: [
+    //     {
+    //       path: '/',
+    //       redirect: '/do_label',
+    //     },
+    //     {
+    //       name: '标注任务管理',
+    //       path: '/label_image/label_task_manage',
+    //       component: './LabelImage/LabelTaskManage',
+    //     },
+    //     {
+    //       name: '蜗牛标注平台',
+    //       path: '/label_image/do_label',
+    //       component: './LabelImage/DoLabel',
+    //     },
+    //   ],
+    // },
     {
-      path: '/LabelTaskManage',
-      component: '@/pages/LabelImage/LabelTaskManage/index',
+      path: '/',
+      redirect: '/label_task_manage',
+    },
+    {
+      name: '标注任务管理',
+      path: '/label_task_manage',
+      component: './LabelImage/LabelTaskManage',
+    },
+    {
+      name: '标注平台',
+      path: '/do_label',
+      component: './LabelImage/DoLabel',
     },
   ],
   fastRefresh: {},
---
+++ src/utils/util.ts
@@ -0,0 +1,9 @@
+export const getBase64 = (file: any) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+    // resolve(reader.result);
+  });
+};
